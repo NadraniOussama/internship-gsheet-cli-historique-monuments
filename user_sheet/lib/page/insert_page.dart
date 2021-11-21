0ec45d5dbb017515c4726a391169c79d15cb9959
oussama <nadranisama@gmail.com>
2021-11-21 14:43:59 +0100

    most likely to estetic change to the code no significant change

--- user_sheet/lib/page/insert_page.dart
+++ user_sheet/lib/page/insert_page.dart
@@ -4,7 +4,6 @@ import 'package:user_sheet/list/users.dart';
 import 'package:user_sheet/model/user.dart';
 
 class InsertPageState extends StatelessWidget {
-
   final _formKey = GlobalKey<FormState>();
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -12,27 +11,25 @@ class InsertPageState extends StatelessWidget {
   TextEditingController nameController = TextEditingController();
   TextEditingController nom = TextEditingController();
   TextEditingController dateConstruction = TextEditingController();
-  TextEditingController epoqueConstruction  = TextEditingController();
-  TextEditingController personneHistorique  = TextEditingController();
+  TextEditingController epoqueConstruction = TextEditingController();
+  TextEditingController personneHistorique = TextEditingController();
   TextEditingController descriptionHistorique = TextEditingController();
   TextEditingController lien = TextEditingController();
   TextEditingController categorie = TextEditingController();
   TextEditingController prixAcces = TextEditingController();
-  TextEditingController surface= TextEditingController();
-  TextEditingController  positionCarte = TextEditingController();
+  TextEditingController surface = TextEditingController();
+  TextEditingController positionCarte = TextEditingController();
   TextEditingController image = TextEditingController();
   late Users user;
   late Color color;
   String buttonName;
   InsertPageState(this.user, this.color, this.buttonName);
 
-
   Future<void> _submitForm() async {
-
-    if(_formKey.currentState!.validate()){
-   // descriptionHistorique,lien,categorie,prixAcces,surface,positionCarte,image
-      final Map<String,dynamic> users = {
-        UserFields.nom : nameController.text,
+    if (_formKey.currentState!.validate()) {
+      // descriptionHistorique,lien,categorie,prixAcces,surface,positionCarte,image
+      final Map<String, dynamic> users = {
+        UserFields.nom: nameController.text,
         UserFields.dateConstruction: dateConstruction.text,
         UserFields.epoqueConstruction: epoqueConstruction.text,
         UserFields.personneHistorique: personneHistorique.text,
@@ -50,18 +47,14 @@ class InsertPageState extends StatelessWidget {
           personneHistorique: personneHistorique.text, descriptionHistorique: descriptionHistorique.text,
           lien: lien.text, categorie: categorie.text, positionCarte: positionCarte.text, image: image.text} as Users ;*/
 
-     // await UserSheetApi.insert([user]);
+      // await UserSheetApi.insert([user]);
       print(users);
-      await UserSheetApi.update("1" ,users);
+      await UserSheetApi.update("1", users);
       print(users);
       await UserSheetApi.init();
 
-
       _showSnackbar("Submitting Feedback");
-
     }
-
-
   }
 
   // Method to show snackbar with 'message'.
@@ -75,168 +68,153 @@ class InsertPageState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key:  _scaffoldKey,
+      key: _scaffoldKey,
       body: Center(
         child: ListView(
           children: [
             Container(
-              padding: EdgeInsets.symmetric(vertical: 50,horizontal: 24),
+              padding: EdgeInsets.symmetric(vertical: 50, horizontal: 24),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
-              TextFormField(
-                //initialValue: this.user.nom,
-                controller: dateConstruction,
-                validator: (value){
-                  if(value!.isEmpty){
-                    return "Enter ValiddateConstruction";
-                  }
-                  return null;
-                },
-              //initialValue: this.user.nom,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: color),
-                    borderRadius: BorderRadius.circular(5.5),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: color,
+                  TextFormField(
+                    //initialValue: this.user.nom,
+                    controller: dateConstruction,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Enter ValiddateConstruction";
+                      }
+                      return null;
+                    },
+                    //initialValue: this.user.nom,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: color),
+                        borderRadius: BorderRadius.circular(5.5),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: color,
+                        ),
+                      ),
+                      filled: true,
+                      //fillColor: Colors.red[50],
+                      labelText: labelText,
+                      labelStyle: TextStyle(color: color),
                     ),
                   ),
-                  filled: true,
-                  //fillColor: Colors.red[50],
-                  labelText: labelText,
-                  labelStyle: TextStyle(color: color),
-                ),
-              ),
                   Form(
                     key: _formKey,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         TextFormField(
-                          controller: null = nameController ? user.nom : "",
-                          validator: (value){
-                            if(value!.isEmpty){
+                          // ignore: unnecessary_null_comparison
+                          controller: (null y
+                          == nameController) ? user.nom : "",
+                          validator: (value) {
+                            if (value!.isEmpty) {
                               return "Enter Valid Name";
                             }
                             return null;
                           },
-                          decoration: InputDecoration(
-                              labelText: "Name"
-                          ),
+                          decoration: InputDecoration(labelText: "Name"),
                         ),
                         TextFormField(
                           controller: dateConstruction,
-                          validator: (value){
-                            if(value!.isEmpty){
+                          validator: (value) {
+                            if (value!.isEmpty) {
                               return "Enter ValiddateConstruction";
                             }
                             return null;
                           },
-                          decoration: InputDecoration(
-                              labelText: "dateConstruction"
-                          ),
+                          decoration:
+                              InputDecoration(labelText: "dateConstruction"),
                         ),
                         TextFormField(
-                          controller: epoqueConstruction ,
-                          validator: (value){
-                            if(value!.isEmpty){
+                          controller: epoqueConstruction,
+                          validator: (value) {
+                            if (value!.isEmpty) {
                               return "Enter Valid Phone Number";
                             }
                             return null;
                           },
-                          decoration: InputDecoration(
-                              labelText: "Phone Number"
-                          ),
+                          decoration:
+                              InputDecoration(labelText: "Phone Number"),
                         ),
                         TextFormField(
-                          controller: personneHistorique ,
-                          validator: (value){
-                            if(value!.isEmpty){
+                          controller: personneHistorique,
+                          validator: (value) {
+                            if (value!.isEmpty) {
                               return "Enter Valid Feedback";
                             }
                             return null;
                           },
-                          decoration: InputDecoration(
-                              labelText: "Feedback"
-                          ),
+                          decoration: InputDecoration(labelText: "Feedback"),
                         ),
                         TextFormField(
                           controller: descriptionHistorique,
-                          validator: (value){
-                            if(value!.isEmpty){
+                          validator: (value) {
+                            if (value!.isEmpty) {
                               return "Enter Valid descriptionHistorique";
                             }
                             return null;
                           },
-                          decoration: InputDecoration(
-                              labelText: "Feedback"
-                          ),
+                          decoration: InputDecoration(labelText: "Feedback"),
                         ),
                         TextFormField(
                           controller: lien,
-                          validator: (value){
-                            if(value!.isEmpty){
+                          validator: (value) {
+                            if (value!.isEmpty) {
                               return "Enter Valid lien";
                             }
                             return null;
                           },
-                          decoration: InputDecoration(
-                              labelText: "Feedback"
-                          ),
+                          decoration: InputDecoration(labelText: "Feedback"),
                         ),
                         TextFormField(
                           controller: categorie,
-                          validator: (value){
-                            if(value!.isEmpty){
+                          validator: (value) {
+                            if (value!.isEmpty) {
                               return "Enter Valid Name";
                             }
                             return null;
                           },
-                          decoration: InputDecoration(
-                              labelText: "Name"
-                          ),
+                          decoration: InputDecoration(labelText: "Name"),
                         ),
                         TextFormField(
                           controller: prixAcces,
-                          validator: (value){
-                            if(value!.isEmpty){
+                          validator: (value) {
+                            if (value!.isEmpty) {
                               return "Enter Valid Email";
                             }
                             return null;
                           },
-                          decoration: InputDecoration(
-                              labelText: "Email"
-                          ),
+                          decoration: InputDecoration(labelText: "Email"),
                         ),
                         TextFormField(
                           controller: surface,
-                          validator: (value){
-                            if(value!.isEmpty){
+                          validator: (value) {
+                            if (value!.isEmpty) {
                               return "Enter Valid Phone Number";
                             }
                             return null;
                           },
-                          decoration: InputDecoration(
-                              labelText: "Phone Number"
-                          ),
+                          decoration:
+                              InputDecoration(labelText: "Phone Number"),
                         ),
                         TextFormField(
-                          controller:  positionCarte,
-                          validator: (value){
-                            if(value!.isEmpty){
+                          controller: positionCarte,
+                          validator: (value) {
+                            if (value!.isEmpty) {
                               return "Enter Valid Feedback";
                             }
                             return null;
                           },
                           decoration: InputDecoration(
-                              prefixText: "esfsfsf",
-                              labelText: "Feedback"
-                          ),
+                              prefixText: "esfsfsf", labelText: "Feedback"),
                         ),
-                       /* TextFormField(
+                        /* TextFormField(
                           controller: image,
                           validator: (value){
                             if(value!.isEmpty){
@@ -268,23 +246,23 @@ class InsertPageState extends StatelessWidget {
 }
 
 Widget buildTextField() => TextFormField(
-  decoration: InputDecoration(
-    focusedBorder: OutlineInputBorder(
-      borderSide: BorderSide(color: Colors.red),
-      borderRadius: BorderRadius.circular(5.5),
-    ),
-    enabledBorder: OutlineInputBorder(
-      borderSide: BorderSide(
-        color: Colors.red,
+      decoration: InputDecoration(
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red),
+          borderRadius: BorderRadius.circular(5.5),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.red,
+          ),
+        ),
+        prefixIcon: Icon(
+          Icons.person,
+          color: Colors.red,
+        ),
+        filled: true,
+        fillColor: Colors.red[50],
+        labelText: "Enter your Name",
+        labelStyle: TextStyle(color: Colors.red),
       ),
-    ),
-    prefixIcon: Icon(
-      Icons.person,
-      color: Colors.red,
-    ),
-    filled: true,
-    fillColor: Colors.red[50],
-    labelText: "Enter your Name",
-    labelStyle: TextStyle(color: Colors.red),
-  ),
-);
+    );
