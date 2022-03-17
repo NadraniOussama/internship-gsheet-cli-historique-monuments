a7ed9227446e415a7962705bffcd947db873dc0f
oussama <nadranisama@gmail.com>
2022-03-17 22:50:45 +0100

    now we can read momumnet name from jsonfile

--- user_sheet/lib/list/serveces.dart
+++ user_sheet/lib/list/serveces.dart
@@ -7,7 +7,7 @@ import 'package:flutter/services.dart' as rootBundle;
 
 class Services {
   static Future<List<Users>> getFeedBackFromSheet(String namePath) async {
-    print('hahna bdina11');
+    // print('hahna bdina11');
     var url =
         "https://script.google.com/macros/s/AKfycbxQ3KRzC3qjxveKy7prjn6O0-tILtUUMpcHhev88x5eQfxTkpv40TiEAjT2T0XQdeA8/exec";
     try {
@@ -15,13 +15,18 @@ class Services {
       var response = await http.get(Uri.parse(url));
       // print('hahna bdina44');
       if (response.statusCode == 200) {
-        // print('hahna bdina55');
         List<Users> users = usersFromJson(response.body);
         List<Users> result = [];
         List<String>? nameMonuments = await getJsonData(namePath);
+        // print('hahna bdina55');
+
         users.forEach((element) {
           print(nameMonuments);
-          if (nameMonuments!.contains(element.nom)) result.add(element);
+          if (nameMonuments!.contains(element.nom)) {
+            print("it is true");
+            result.add(element);
+          }
+          print("loop ${element.nom}");
         });
         print("goooooooooooooooooood");
         return result;
@@ -35,16 +40,22 @@ class Services {
   }
 
   static Future<List<String>?> getJsonData(String namePath) async {
+    print("test 1");
     final jsonData =
         await rootBundle.rootBundle.loadString('assets/places.json');
+    print("test 2");
     final list = json.decode(jsonData) as List<dynamic>;
+    print("test 3");
     List<Path> paths = list.map((e) => Path.fromJson(e)).toList();
+    print("test 4");
     var lists;
+    print("test 5");
     paths.forEach((element) {
       if (element.pathName == namePath) {
         lists = element.list!;
       }
     });
+    print("hey we donezo");
     return lists;
   }
 }
--- user_sheet/lib/model/path.dart
+++ user_sheet/lib/model/path.dart
@@ -1,10 +1,16 @@
 class Path {
   String? pathName;
+  int numberMon = 10;
   List<String>? list;
   Path({this.pathName, this.list});
   Path.fromJson(Map<String, dynamic> json) {
     pathName = json["path"];
-    list = json["list"];
+    numberMon = json["size"];
+    list = [];
+    for (int i = 0; i < numberMon; i++) {
+      list!.add(json["list"][i]);
+      // print("This is element $i hey: ${list!.elementAt(i)}");
+    }
   }
   String? getName() {
     return (pathName == null) ? pathName : "";
